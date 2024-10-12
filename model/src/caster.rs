16ecb9c7659554f88bc612221afe8fc93874f7ff
GIUSEPPE BARONE s332157 <s332157@studenti.polito.it>
2024-10-13 01:52:54 +0200

    feat:screen sharing implemented
    feat: implementation of the model caster and viewer

---
+++ model/src/caster.rs
@@ -0,0 +1,6 @@
+//Caster must have a IP address and able to start a screen sharing
+struct Caster{
+    name:String,
+    username:String,
+    ip_address:String
+}
---
+++ model/src/viewer.rs
@@ -0,0 +1,20 @@
+//viewer must have the ip address of the caster, and must have the possibility to see the screen sharing (create a methosd for watching a streaming video)
+//for now ip address is a string, later will have to change
+struct Viewer{
+    name:String,
+    username:String,
+    ip_address:String
+}
+
+impl Viewer {
+    //maybe it must change
+    pub fn watch_stream(){
+
+    }
+    pub fn receive_stream(){
+
+    }
+
+
+
+}
